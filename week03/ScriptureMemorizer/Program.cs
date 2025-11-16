dcfc68d6f3184a86c19bb31cbef471c1d9c1df88
alighieri07 <alighieri.vergara@gmail.com>
2025-11-15 21:15:06 -0300

    Scripture Memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,167 @@
-using System;
+using System.Collections.Generic;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        foreach (string word in text.Split(' '))
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        int hidden = 0;
+        List<int> availableIndexes = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (!_words[i].IsHidden())
+                availableIndexes.Add(i);
+        }
+        while (hidden < numberToHide && availableIndexes.Count > 0)
+        {
+            int idx = rand.Next(availableIndexes.Count);
+            _words[availableIndexes[idx]].Hide();
+            availableIndexes.RemoveAt(idx);
+            hidden++;
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+        foreach (Word w in _words)
+        {
+            displayWords.Add(w.GetDisplayText());
+        }
+        return $"{_reference.GetDisplayText()} {string.Join(" ", displayWords)}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word w in _words)
+        {
+            if (!w.IsHidden())
+                return false;
+        }
+        return true;
+    }
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int? _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = null;
+    }
+
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_endVerse.HasValue)
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string text = "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.";
+
+        Scripture scripture = new Scripture(reference, text);
+
+        int wordsToHideEachTime = 3;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.Write("Press ENTER to continue or type 'quit' to exit: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine("\nAll words have been hidden! End of program.");
+                break;
+            }
+
+            scripture.HideRandomWords(wordsToHideEachTime);
+        }
     }
 }
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
+
